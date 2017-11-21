415ee364c0c2f9359aaed097b085b0a922e9f810
Henry MATHEISEN <henrymatheisen@MacBook-Pro-de-Henry.local>
2017-11-21 14:29:38 +0100

    corrections

--- app/Vues/vueAccueil.php
+++ app/Vues/vueAccueil.php
@@ -9,14 +9,14 @@
     <li>A Propos</li>
 </ul>
 
-    <TABLE border="1" width="100%">
-        <TR>
-            <TD><h1>Découvrez nos tout nouveaux produits</h1>
+    <table border="1" width="100%">
+        <tr>
+            <td><h1>Découvrez nos tout nouveaux produits</h1>
                 <img class="pub" src="../public/Capteur_infrarouge.png">
                 <p><a href="https://www.generationrobots.com/fr/400833-capteur-de-distance-infrarouge.html">En savoir plus</a></p>
-            </TD>
-        </TR>
-        <TR>
-            <TD>Ligne 2, colonne 1</TD>
-        </TR>
-    </TABLE>
+            </td>
+        </tr>
+        <tr>
+            <td>Ligne 2, colonne 1</td>
+        </tr>
+    </table>
