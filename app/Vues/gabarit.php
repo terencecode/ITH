180980e181fc1aaf90bdb384b268a80052bf8dc5
Cyprien Lambert <cyprien.lambert@hotmail.fr>
2017-11-27 16:05:58 +0100

    Intégration framework et header

--- app/Vues/gabarit.php
+++ app/Vues/gabarit.php
@@ -5,7 +5,9 @@
 
     <meta charset="UTF-8" />
 
-    <link rel="stylesheet" href="../public/css/style.css">
+
+    <link rel="stylesheet" href="../public/css/framework.css">
+      <link rel="stylesheet" href="../public/css/header.css">
     <?= $css ?>
 
     <title>
@@ -16,7 +18,15 @@
 
 
   <body>
-
+  <div id="header" class="row">
+      <div class="col-md-1 col-lg-1"><a href="#"><img id="logo" src="../public/images/logo.png"></a></div>
+      <div class="col-md-2 col-lg-1 header-link"><a href="#">Accueil</a></div>
+      <div class="col-md-2 col-lg-1 header-link"><a href="#">Connexion</a></div>
+      <div class="col-md-2 col-lg-1 header-link"><a href="#">S'enregistrer</a></div>
+      <div class="col-md-2 col-lg-6"></div>
+      <div class="col-md-2 col-lg-1 header-link"><a href="#">A Propos</a></div>
+      <div class="col-md-1 col-lg-1"><a href="#"><img id="help" src="../public/images/help.png"></a></div>
+  </div>
     <?= $contenu ?>
 
   </body>
--- app/Vues/vueEnregistrement.php
+++ app/Vues/vueEnregistrement.php
@@ -9,18 +9,32 @@
   }
 } ?>
 
-<form action="" class="enregistrement" method="post">
-  Nom:<br>
-  <input type="text" name="nom"><br>
-  Prénom:<br>
-  <input type="text" name="prenom"><br>
-  Adresse Mail:<br>
-  <input type="text" name="mail"><br>
-  Mot de Passe:<br>
-  <input type="password" name="passe"><br>
-  Répéter le mot de passe:<br>
-  <input type="text" name="passe2"><br>
-  Code d'inscription:<br>
-  <input type="text" name="code"><br>
-  <input type="submit" name='valider'>
-</form>
+<div id="body">
+    <div class="row">
+
+        <div id="background-image" class="col-xs-hide col-sm-4 col-md-7 col-lg-9"></div>
+
+        <div id="form-container" class="col-xs-12 col-sm-8 col-md-5 col-lg-3">
+            <h1>Créer un compte</h1>
+            <form action="">
+                <label type="prenom">Prénom:</label>
+                <input type="text" name="prenom" id="prenom"></br>
+                <label for="nom">Nom:</label>
+                <input type="text" name="nom" id="nom"></br>
+                <label for="mail">Adresse mail:</label>
+                <input type="text" name="mail" id="mail"></br>
+                <label for="mdp">Mot de passe:</label>
+                <input type="password" name="passe" id="mdp"
+                       placeholder="10 caractères minimum"></br>
+                <label for="mdp2">Répeter mot de passe:</label>
+                <!-- Penser à tester si le mdp est identique-->
+                <input type="password" name="passe2" id="mdp2"></br>
+                <label for="code">Code d'inscription:</label>
+                <input type="text" name="valider" id="code"></br>
+
+                <input id="submit-button" type="submit" value="valider">
+            </form>
+        </div>
+
+    </div>
+</div>
