660a917dcc71c1635f2bcfc50cb46358b4cd3293
Cyprien Lambert <cyprien.lambert@hotmail.fr>
2017-12-12 08:45:09 +0100

    travail sur la page ma maison

--- app/Vues/vueApropos.php
+++ app/Vues/vueApropos.php
@@ -4,31 +4,31 @@
 <div>
     <table>
         <tr>
-            <td id="nom">MATHEISEN Henry</td>
+            <td id="mail">MATHEISEN Henry</td>
         </tr>
         <tr>
             <td id="mail">henry.matheisen@isep.fr</td>
         </tr>
         <tr>
-            <td id="nom">LAMBERT Cyprien</td>
+            <td>LAMBERT Cyprien</td>
         </tr>
         <tr>
             <td id="mail">cyprien.lambert@isep.fr</td>
         </tr>
         <tr>
-            <td id="nom">Lathiere Alexandre</td>
+            <td>Lathiere Alexandre</td>
         </tr>
         <tr>
             <td id="mail">alexandre.lathiere@isep.fr</td>
         </tr>
         <tr>
-            <td id="nom">LOURTET Jules</td>
+            <td>LOURTET Jules</td>
         </tr>
         <tr>
             <td id="mail">jules.lourtet@isep.fr</td>
         </tr>
         <tr>
-            <td id="nom">TROJANOWSKI William</td>
+            <td>TROJANOWSKI William</td>
         </tr>
         <tr>
             <td id="mail">william.trojanowski@isep.fr</td>
