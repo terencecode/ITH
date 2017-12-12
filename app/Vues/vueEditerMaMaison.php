7da4cbef4ef5362a10af859263daef4d29b0d18d
Henry MATHEISEN <henrymatheisen@MacBook-Pro-de-Henry.local>
2017-12-12 09:58:14 +0100

    modal 2

--- app/Vues/vueEditerMaMaison.php
+++ app/Vues/vueEditerMaMaison.php
@@ -11,8 +11,10 @@
             <h2>Modal Example</h2>
 
             <!-- Trigger/Open The Modal -->
-            <button class="button" type="button" id="button_piece">Ajouter une pièce</button></br>
-            <button class="button" type="button" id="button_capteur">Ajouter un capteur</button></br>
+            <button class="button" type="button" id="button_piece">Ajouter une pièce</button>
+            </br>
+            <button class="button" type="button" id="button_capteur">Ajouter un capteur</button>
+            </br>
 
             <!-- The Modal -->
             <div id="modal_piece" class="modal">
@@ -24,19 +26,19 @@
                     <form action="#" method="post">
                         <label for="dimensions"><p>Dimensions :</p></label>
                         <input type="text" name="longueur"
-                            placeholder="longueur"></br>
+                               placeholder="longueur"></br>
                         <input type="text" name="largeur"
                                placeholder="largeur"></br>
                         <input type="text" name="hauteur"
                                placeholder="hauteur"></br>
                         <label for="type_de_piece"><p>Type de pièce :</p></label>
-                            <select>
-                                <option value="chambre">Chambre</option>
-                                <option value="cuisine">Cuisine</option>
-                                <option value="bureau">Bureau</option>
-                                <option value="salon">Salon</option>
-                                <option value="autre">Autre</option>
-                            </select>
+                        <select>
+                            <option value="chambre">Chambre</option>
+                            <option value="cuisine">Cuisine</option>
+                            <option value="bureau">Bureau</option>
+                            <option value="salon">Salon</option>
+                            <option value="autre">Autre</option>
+                        </select>
                         <label for="emplacement"><p>Emplacement :</p></label>
                         <input type="text" name="emplacement"></br>
                         <label><p>Description :</p></label></br>
@@ -45,38 +47,10 @@
                         <input id="submit-button" name="valider" type="submit" value="Annuler">
                     </form>
 
-                    </form>
 
                 </div>
+
             </div>
-                <script>
-                    // Get the modal
-                    var modal = document.getElementById('modal_piece');
-
-                    // Get the button that opens the modal
-                    var btn = document.getElementById("button_piece");
-
-                    // Get the <span> element that closes the modal
-                    var span = document.getElementsByClassName("close")[0];
-
-                    // When the user clicks the button, open the modal
-                    btn.onclick = function() {
-                        modal.style.display = "block";
-                    };
-
-                    // When the user clicks on <span> (x), close the modal
-                    span.onclick = function() {
-                        modal.style.display = "none";
-                    }
-
-                    // When the user clicks anywhere outside of the modal, close it
-                    window.onclick = function(event) {
-                        if (event.target == modal) {
-                            modal.style.display = "none";
-                        }
-                    }
-                </script>
-
             <!-- Modale des capteurs-->
 
 
@@ -96,12 +70,53 @@
                         <textarea name="Question" rows="4" cols="40"></textarea>
                     </form>
 
-                    </form>
-
                 </div>
 
             </div>
             <!-- Ajouter la partie "script" pour créer la modal, mais le copié/collé ne fonctinne pas-->
+            <script>
+                // Get the modals
+                var modal = document.getElementById('modal_piece');
+                var modal2 = document.getElementById('modal_capteur');
+
+                // Get the buttons that opens the modal
+                var btn = document.getElementById("button_piece");
+                var btn2 = document.getElementById("button_capteur");
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+                var span2 = document.getElementsByClassName("close")[1];
+
+
+                // When the user clicks the buttons, open the modals
+                btn.onclick = function () {
+                    modal.style.display = "block";
+                };
+
+                btn2.onclick = function () {
+                    modal2.style.display = "block"
+                };
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function () {
+                    modal.style.display = "none";
+                };
+
+                span2.onclick = function () {
+                    modal2.style.display = "none";
+                };
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                    if (event.target == modal2) {
+                        modal2.style.display = "none";
+                    }
+                }
+            </script>
+
         </div>
 
     </div>
