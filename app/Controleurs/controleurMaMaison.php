13dca424442cb7889fb915bd2bd83462ef0b3292
Jules-Lourtet <jules.lourtet1@gmail.com>
2017-12-18 11:55:52 +0100

    Controleur Capteur Model Capteur Timestamp(s) par défaut : "NULL" - à modifier

--- app/Controleurs/controleurMaMaison.php
+++ app/Controleurs/controleurMaMaison.php
@@ -4,6 +4,57 @@ require_once 'Vues/vue.php';
 
 class ControleurMaMaison{
 
+    public function affichageCapteurs(){
+
+        //Gestion des messagges d'erreur si l'utilisateur ne remplis pas les champs
+        //Sera remplacé par du js
+        if (isset($_POST['valider'])) {
+
+            if (empty($_POST['capteur'])) {
+                $this->erreur[] = "Veuillez saisir le nom du capteur";
+            }
+
+            if (empty($_POST['id_piece'])) {
+                $this->erreur[] = "Veuillez saisir la piece";
+            }
+
+            if (empty($_POST['on_off'])) {
+                $this->erreur[] = "Veuillez saisir l'etat du capteur";
+            }
+
+
+            //On stock les valeurs de l'utilisateur pour les passer au modèle ensuite
+            $valeurs = [];
+            $valeurs[] = $_POST['capteur'];
+            $valeurs[] = $_POST['id_piece'];
+            $valeurs[] = $_POST['on_off'];
+
+            //On essaye de rentrer l'utilisateur dans la bdd
+            //On créé les variables de session
+            //Si l'utilisateur existe déjà: retourne une erreur
+            try {
+
+                $capteur = new Capteurs();
+                $capteur->enregistrerCapteurs($valeurs);
+
+            } catch (Exception $e) {
+
+                $this->erreur[] = "Vous avez déjà enregistré ce capteur, veuillez en saisir un nouveau";
+
+                $vue = new Vue('TableauDeBord');
+                $vue->generer(array('erreur' => $this->erreur));
+
+            }
+
+
+        }
+
+        //On genère la vue Enregistrement
+        $vue = new Vue('Enregistrement');
+        $vue->generer(array('erreur' => $this->erreur, 'messagePassesDifferents' => $this->messagePassesDifferents));
+
+    }
+
   public function affichageStatistiques(){
     if (!empty($_SESSION['email']) && !empty($_SESSION['passe'])) {
       $vue = new Vue('Statistiques');
@@ -15,6 +66,7 @@ class ControleurMaMaison{
   }
 
   public function affichageTableauDeBord(){
+
     if (!empty($_SESSION['email']) && !empty($_SESSION['passe'])) {
       $vue = new Vue('TableauDeBord');
       $vue->generer();
--- app/Modeles/capteurs.php
+++ app/Modeles/capteurs.php
@@ -3,28 +3,42 @@
 require_once "Modeles/modele.php";
 
 
-class Capteurs extends Modele {
+class Capteurs extends Modele
+{
 
 
-    public function afficherCapteur($id_ca){
+    public function afficherCapteur($id_ca)
+    {
 
-        $sql = "SELECT id_ca FROM capteur WHERE id_ca = :id_cap";
+        $sql = "SELECT id_ca FROM capteur WHERE id_ca = :id_ca";
         $resultatRequete = $this->executerRequete($sql, array('capteur' => $id_ca))->fetch();
 
         return $resultatRequete;
 
     }
 
-    public function afficherPiece($id_piece){
+    public function afficherPiece($id_piece)
+    {
 
         $sql = "SELECT id_piece FROM capteur WHERE id_piece=:id_piece";
         $resultatRequete = $this->executerRequete($sql, array('piece' => $id_piece))->fetch();
+    }
 
-    public function afficherEtat($on_off){
+    public function afficherEtat($on_off)
+    {
             $sql = "SELECT power_state FROM capteur WHERE power_state = :on_off";
             $resultatRequete = $this->executerRequete($sql, array('on_off' => $on_off))->fetch();
 
         return $resultatRequete;
     }
 
+    public function enregistrerCapteur($valeurs)
+    {
+        $sql = "INSERT INTO capteur (id_capteur, power_state, id_piece) VALUES ('$valeurs[0]', '$valeurs[1]', '$valeurs[2]')";
+        $resultatrequete = $this->executerRequete($sql);
+        return $resultatrequete;
+    }
+
+
 }
+?>
