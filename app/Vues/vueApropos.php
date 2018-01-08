817fd43f9de16cd3e966f9f48eb4afa3b1d23ad6
Cyprien Lambert <cyprien.lambert@hotmail.fr>
2018-01-08 14:56:41 +0100

    footer de la page a propos à checker

--- app/Vues/vueApropos.php
+++ app/Vues/vueApropos.php
@@ -2,45 +2,32 @@
 <?php $this->titre = "A Propos"; ?>
 
 <div id="body">
-    <div class="row">
+    <div class="row infos">
         <div id="form-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <!--Tableau nom/mail-->
-            <table>
-                <tr>
-                    <td><h1>Equipe Domisep</h1></td>
-                </tr>
-                <tr>
-                    <td class="equipe">MAUREL Denis</br>
-                        <p class="mail">Maurel.Denis@domisep.fr</p></td>
-                </tr>
-                <tr>
-                    <td class="equipe">DUPONT Dupond</br>
-                        <p class="mail">dupont.dupond@domisep.fr</p></td>
-                </tr>
-                <tr>
-                    <td class="equipe">DUPOND Dupont</br>
-                        <p class="mail">dupond.dupont@domisep.fr</p></td>
-                </tr>
-                <tr>
-                    <td class="equipe">DUPONT Dupont</br>
-                        <p class="mail">dupont.dupont@domisep.fr</p></td>
-                </tr>
-                <tr>
-                    <td class="equipe">DUPOND Dupond</br>
-                        <p class="mail">dupond.dupond@domisep.fr</p></td>
-                </tr>
+            <div>
+                    <h1>Equipe Domisep</h1>
+                    <div class="equipe">
+                        MAUREL Denis</br>
+                        <div class="mail">maurel.denis@domisep.fr</div>
+                    </div>
+                    <div class="equipe">DUPONT Dupond</br>
+                        <div class="mail">dupont.dupond@domisep.fr</div>
+                    </div>
+                    <div class="equipe">DUPOND Dupont</br>
+                        <div class="mail">dupond.dupont@domisep.fr</div>
+                    </div>
+                    <div class="equipe">DUPONT Dupont</br>
+                        <div class="mail">dupont.dupont@domisep.fr</div>
+                    </div>
+                    <div class="equipe">DUPOND Dupond</br>
+                        <div class="mail">dupond.dupond@domisep.fr</div>
+                    </div>
 
-            </table>
-
-        </div>
-    </div>
-    <div class="row 2">
-        <div id="form-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="footer">
-                <p>
-                    Copyright © 2018 | Designed by LAMBERT Cyprien, LATHIERE Alexandre, LOURTET Jules, MATHEISEN Henry, TROJANOWSKI William
-                </p>
             </div>
         </div>
+    </div>
+    <div class="footer">
+        Copyright © 2018 | Designed by LAMBERT Cyprien, LATHIERE Alexandre, LOURTET Jules, MATHEISEN Henry, TROJANOWSKI William
     </div>
 </div>
