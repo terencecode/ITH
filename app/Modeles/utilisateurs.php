ce127d1822664f3d8fa8d459399abe98ec10a3b1
hmatheisen <henry.mthsn@gmail.com>
2018-01-12 14:45:01 +0100

    corrections

--- app/Modeles/utilisateurs.php
+++ app/Modeles/utilisateurs.php
@@ -4,21 +4,33 @@ require_once "Modeles/modele.php";
 
 class Utilisateurs extends Modele {
 
+  public function chercherId($email){
+    $sql = "SELECT id_u FROM utilisateur WHERE email_u = :email";
+    $resultatRequete = $this->executerRequete($sql, array('email' => $email))->fetch();
+    return $resultatRequete;
+  }
 
-    public function chercherUtilisateur($email){
+    public function chercherUtilisateur($id_u){
 
-      $sql = "SELECT email_u, mdp_u FROM utilisateur WHERE email_u = :email";
-      $resultatRequete = $this->executerRequete($sql, array('email' => $email))->fetch();
+      $sql = "SELECT email_u, mdp_u FROM utilisateur WHERE id_u = :id_u";
+      $resultatRequete = $this->executerRequete($sql, array('id_u' => $id_u))->fetch();
       return $resultatRequete;
 
   }
+
+  public function chercherAdmin($id_u){
+
+    $sql = "SELECT id_u FROM gerant WHERE id_u = :id_u";
+    $resultatRequete = $this->executerRequete($sql, array('id_u' => $id_u))->fetch();
+    return $resultatRequete;
+
+}
 
   public function chercherUtilisateurs(){
     $sql = "SELECT email_u, prenom_u, nom_u FROM utilisateur";
     $resultatRequete = $this->executerRequete($sql)->fetchAll(PDO::FETCH_ASSOC);
     return $resultatRequete;
   }
-
 
     public function enregistrerUtilisateur($valeurs){
 
