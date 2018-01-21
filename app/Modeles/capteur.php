151f1573d589681ed2f07267710e5a8be8d278af
reseau-et-securite <alexandre.lat@gmail.com>
2018-01-21 11:21:20 +0100

    Push

---
+++ app/Modeles/capteur.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once "../../../ITH/app/Modeles/modele.php";
+
+
+class Capteur extends Modele
+{
+
+    public function GetValues($id_ca,$TypeCapteur)
+    {
+        $sql = "SELECT  valeur , timestamp FROM ";
+        $sql  = $sql  . $TypeCapteur;
+        $sql  = $sql  . " WHERE id_ca=";
+        $sql  = $sql.$id_ca;
+        $sql  = $sql ." ORDER BY timestamp";
+
+      //  $resultatRequete = $this->executerRequete($sql, array('id_ca' => $id_ca))->fetchAll(PDO::FETCH_ASSOC);
+        $resultat = $this->executerRequete($sql, null);
+
+
+
+        echo "<list>";
+
+        while ($Tab_result= $resultat->fetch()) {
+
+            $item = "<item valeur=\"" .$Tab_result["valeur"] . "\" timestamp=\"" . $Tab_result["timestamp"] . "\" />";
+            //echo  $Tab_result["nom"];
+            echo  $item;
+
+        }
+        echo "</list>";
+    }
+
+
+
+
+
+}
+?>
